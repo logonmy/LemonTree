0f428b2db9c75a6f0e936ec133f4db029fd18819
niusheng <niusheng@cffex.com.cn>
2015-07-03 14:33:02 +0800

    modify hostinfo pages

--- static/jquery/hostinfo.js
+++ static/jquery/hostinfo.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
     for(var i=1; i<$cilist.length; i++)
     {
         $('#'+$cilist[i]).dataTable( {
+            "bAutoWidth": true,                                        //页面自动宽度
             "processing" : true,
-            "bPaginate": false,
-            "sDom" : '<"toolbar_'+ $cilist[i] +'">frti',
+            "bPaginate": false,                                        //页面分页（右下角）
+            "sDom" : '<"toolbar_'+ $cilist[i] +'">frti',               //自定义组件排版
             "ajax" : {
                 "url" : "/getci?cifid=" + $cilist[i],
                 "dataSrc" : "",
@@ -17,11 +18,26 @@ $(document).ready(function(){
                 { "data": "CIAT_NAME" },
                 { "data": "VALUE" },
                 { "data": "DESCRIPTION" },
-                { "data": "CI_NAME", "bVisible" : false}
-            ]
+                { "data": "CI_NAME" },
+                //, "bVisible" : false}
+            ],
+            "oLanguage": {                                             //自定义内容——国际化
+                "sLengthMenu": "每页显示 _MENU_ 条记录",
+                "sZeroRecords": "抱歉， 没有找到",
+                //"sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+                "sInfo": "共 _TOTAL_ 个属性",
+                "sInfoEmpty": "没有数据",
+                "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+                "oPaginate": {
+                    "sFirst": "首页" ,
+                    "sPrevious": "前一页",
+                    "sNext": "后一页",
+                    "sLast": "尾页"
+                },
+                "sZeroRecords": "没有检索到数据",
+            }
         } );
-        
-        $("div.toolbar_" + $cilist[i]).html();
+       
     }
     
     
